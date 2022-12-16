6d04cf431f7275cf4ff1da5c7781d21d3a7c9180
Thomas Barreto <thomi.b137@gmail.com>
2022-12-16 20:25:40 -0300

    db-connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      port: 3306,
+      database: 'apinestdb',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
